ccaeb1dfb4302d514ec4212411b5dcd7c3f96011
AmandhaYumi <mandhayumi@gmail.com>
2026-05-19 09:37:25 -0300

    feat:corrigndo routes

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import Cadastro from "./src/screens/Cadastro";
-import Perfil from "./src/screens/Perfil";
-import Devs from "./src/screens/Devs";
-
-import TabBar from "./src/components/TabBar";
 import { UserProvider } from "./src/context/UserContext";
-
-const Tab = createBottomTabNavigator();
+import TabRoutes from "./src/routes/tab.routes";
 
 export default function App() {
   return (
     <UserProvider>
       <NavigationContainer>
-        <Tab.Navigator
-          tabBar={(props) => <TabBar {...props} />}
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Tab.Screen name="Cadastro" component={Cadastro} />
-          <Tab.Screen name="Perfil" component={Perfil} />
-          <Tab.Screen name="Devs" component={Devs} />
-        </Tab.Navigator>
+        <TabRoutes />
       </NavigationContainer>
     </UserProvider>
   );
